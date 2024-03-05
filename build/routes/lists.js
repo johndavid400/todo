88895d8204a939bd2df92259e48e82b282809705
JD Warren <jd.warren@unosquare.com>
2024-03-05 07:50:02 -0600

    re-arrange files and get docker working again

--- build/routes/lists.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.lists = void 0;
-const express_1 = require("express");
-exports.lists = (0, express_1.Router)();
-exports.lists.get('/', (req, res) => {
-    res.send('hey');
-});
