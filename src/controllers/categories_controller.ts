aee6692057dae64f00ffbae8c67c7f9e8ad3c2ad
JD Warren <jd.warren@unosquare.com>
2024-03-05 09:56:43 -0600

    add prisma

--- src/controllers/categories_controller.ts
+++ src/controllers/categories_controller.ts
@@ -1,11 +1,20 @@
 import { Request, Response } from 'express';
+import { PrismaClient } from '@prisma/client'
 
-export function getCategories(req: Request, res: Response) {
-  return res.json({ message: 'Get Categories' }).status(200);
+const prisma = new PrismaClient()
+
+export async function getCategories(req: Request, res: Response) {
+  const categories = await prisma.categories.findMany();
+  return res.json(categories).status(200);
 }
 
-export function getCategory(req: Request, res: Response) {
-  return res.json({ message: 'Get Category', params: req.params }).status(200);
+export async function getCategory(req: Request, res: Response) {
+  const category = await prisma.categories.findFirst({
+    where: {
+      id: parseInt(req.params.id)
+    },
+  });
+  return res.json(category).status(200);
 }
 
 export function createCategory(req: Request, res: Response) {
