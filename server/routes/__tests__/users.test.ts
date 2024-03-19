227f1b4fd662868b4e8317f3e7fb89d0ab354ac9
JD Warren <jd.warren@unosquare.com>
2024-03-19 16:03:56 -0500

    update user tests

--- server/routes/__tests__/users.test.ts
+++ server/routes/__tests__/users.test.ts
@@ -3,6 +3,21 @@ import request from 'supertest';
 import { faker } from '@faker-js/faker';
 
 describe('/users', () => {
+  describe('GET /users', () => {
+    it('should return users', async () => {
+      await request(app)
+        .get('/users')
+        .expect(200);
+    });
+  });
+  describe('GET /users/1', () => {
+    it('should return a user', async () => {
+      await request(app)
+        .get('/users')
+        .send('1')
+        .expect(200);
+    });
+  });
   describe('POST /users', () => {
     it('should return success', async () => {
       const user = {
@@ -17,4 +32,21 @@ describe('/users', () => {
         .expect(200);
     });
   });
+  /*
+  describe('PUT /users/1', () => {
+    it('should return success', async () => {
+      await request(app)
+        .pute('/users/1')
+        .send({ password: 'testpassword' })
+        .expect(200);
+    });
+  });
+  describe('DELETE /users/1', () => {
+    it('should return success', async () => {
+      await request(app)
+        .delete('/users/1')
+        .expect(200);
+    });
+  });
+  */
 });
