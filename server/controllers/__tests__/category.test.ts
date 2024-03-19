e22be88943b54eeba5661df7f770e5b651648833
JD Warren <jd.warren@unosquare.com>
2024-03-19 10:21:02 -0500

    fix category test

--- server/controllers/__tests__/category.test.ts
+++ server/controllers/__tests__/category.test.ts
@@ -2,24 +2,20 @@ import { when } from "jest-when";
 import * as categoriesController from '../categories_controller';
 import { mockRequest, mockResponse } from '../../testUtils/mock_request';
 import { prisma } from "../../utils/prisma";
-//import { prismaMock } from "./../../testUtils/prisma";
+import { prismaMock } from "./../../testUtils/prisma";
 
-jest.mock("@prisma/client");
-jest.mock("../../utils/prisma");
+jest.mock('@prisma/client');
 
 describe("categories controller", () => {
-  it.skip("Should return 200 when categories available", async () => {
-    /*
+  it("Should return 200 when categories available", async () => {
     const userId = 1;
     const req = mockRequest({ locals: { userId: userId } });
     const res = mockResponse(userId);
-    const mockReturnValue = [] as any;
 
-    prisma.categories = { findMany: jest.fn().mockReturnValueOnce(mockReturnValue) };
+    const categories = [{ id: 1 }];
+    prismaMock.categories = { findMany: jest.fn().mockReturnValueOnce(categories) };
 
-    await categoriesController.getCategories(req, res);
-
-    expect(res.sendStatus).toHaveBeenCalledTimes(1);
-    */
+    const result = await categoriesController.getCategories(req, res);
+    expect(prisma.categories.findMany).toHaveBeenCalledTimes(1);
   });
 });
