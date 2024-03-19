95e8f6a09e477942799818561bcbfba627c99c35
JD Warren <jd.warren@unosquare.com>
2024-03-19 15:41:01 -0500

    finish user tests

--- server/controllers/users_controller.ts
+++ server/controllers/users_controller.ts
@@ -15,29 +15,16 @@ export async function getUser(req: Request, res: Response) {
 
 export async function createUser(req: Request, res: Response) {
   const { email, password } = req.body;
-  const hashedPassword = await bcrypt.hash(password, 10);
-
-  const user = await prisma.users.create({
-    data: {
-      email: email,
-      encrypted_password: hashedPassword
-    },
-  });
+  const user = userService.createUser(email, password);
   return res.json(user).status(200);
 }
 
 export async function updateUser(req: Request, res: Response) {
   const { id } = req.params;
   const { password } = req.body;
-  const hashedPassword = await bcrypt.hash(password, 10);
 
   try {
-    const user = await prisma.users.update({
-      where: { id: Number(id) },
-      data: {
-        encrypted_password: hashedPassword
-      },
-    })
+    const user = userService.updateUser(Number(id), password);
     return res.json(user).status(200);
   } catch (error) {
     res.json({ error: `User with ID ${id} does not exist in the database` })
@@ -46,10 +33,6 @@ export async function updateUser(req: Request, res: Response) {
 
 export async function deleteUser(req: Request, res: Response) {
   const { id } = req.params;
-  const user = await prisma.users.delete({
-    where: {
-      id: Number(id)
-    },
-  })
+  const user = userService.deleteUser(Number(id));
   return res.json(user).status(200);
 }
