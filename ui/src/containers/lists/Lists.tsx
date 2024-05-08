ea2954531b92869dd0ad861f6a32ec4ad9443e01
JD Warren <jd.warren@unosquare.com>
2024-05-07 21:05:30 -0500

    update style

--- ui/src/containers/lists/Lists.tsx
+++ ui/src/containers/lists/Lists.tsx
@@ -3,19 +3,29 @@ import { useState, useEffect } from 'react';
 import instance from "@/utils/axios";
 import ListCard from '@/components/ListCard';
 import { Input } from "@/components/ui/input"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+
 
 const Lists = () => {
   const navigate = useNavigate();
   const [lists, setLists] = useState();
   const [list, setList] = useState();
-  //const [categories, setCategories] = useState();
+  const [category, setCategory] = useState(1);
+  const [categories, setCategories] = useState();
 
   const addList = async () => {
     const title = list;
     const input = document.getElementById("new-input");
+    const category_id = category;
     input.value = '';
     return await instance
-      .post("/lists", { title })
+      .post("/lists", { title, category_id })
       .then((response) => {
         getLists();
       });
@@ -29,20 +39,34 @@ const Lists = () => {
       });
   };
 
+  const getCategories = async () => {
+    return await instance
+      .get("/categories/")
+      .then((response) => {
+        setCategories(response.data);
+      });
+  };
+
   const getAll = async () => {
     await getLists(); 
+  };
+
+  const handleCategory = (value: any) => {
+    console.log(value);
+    setCategory(value);
   };
 
   const editable = false;
 
   useEffect(() => {
     getAll();
+    getCategories();
   }, []);
 
   return (
     <>
       <h1>Lists</h1>
-      <div className="lists flex flex-row items-start">
+      <div className="lists">
         {lists && lists.sort((a, b) => (a.id > b.id) ? 1 : -1).map(function(i){
           return (
             <div key={i.id} className="list-container flex items-center justify-between space-x-2 mt-3 me-3">
@@ -53,6 +77,18 @@ const Lists = () => {
       </div>
       <div className="flex flex-col mt-5">
         <div id="new" className="flex ">
+          <Select onValueChange={(value) => handleCategory(value)} >
+            <SelectTrigger className="w-[180px]">
+              <SelectValue placeholder={'Category'} />
+            </SelectTrigger>
+            <SelectContent>
+              {categories && categories.sort((a, b) => (a.id > b.id) ? 1 : -1).map(function(i){
+                return (
+                  <SelectItem value={i.id}>{i.title}</SelectItem>
+                )
+              })}
+            </SelectContent>
+          </Select>
           <Input id="new-input" onChange={(e) => setList(e.target.value)} />
           <input type="button" id="new-btn" value="Add" onClick={() => addList()} />
         </div>
