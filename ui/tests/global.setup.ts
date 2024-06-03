e4def8e17e1cfbf7c782908724b39c9727ddb4b8
JD Warren <jd.warren@unosquare.com>
2024-06-03 13:28:47 -0500

    WIP - fe test containers

--- ui/tests/global.setup.ts
+++ ui/tests/global.setup.ts
@@ -1,18 +1,36 @@
 import { test as setup } from '@playwright/test';
-import * as path from 'path';
+import { DockerComposeEnvironment, Wait } from "testcontainers";
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 setup('create new database', async ({ }) => {
   console.log('creating new database...');
 
+  console.log('start');
+
+  const filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+  const dirname = path.dirname(filename);
+
   // Initialize the database
-  const composeFilePath = path.resolve(__dirname, "../");
-      const composeFile = "docker-compose.yml";
-      composeContainer = await new DockerComposeEnvironment(
+  const composeFilePath = path.resolve(dirname, "../../");
+  const composeFile = "docker-compose.yml";
+
+  console.log('go');
+
+  try {
+    const composeContainer = await new DockerComposeEnvironment(
         composeFilePath,
         composeFile
       )
-        .withProfiles("dev", "fe")
-        .withWaitStrategy("api-1", Wait.forLogMessage(/^Server started on 5000/))
-        .up();
-      await new Promise((x) => setTimeout(x, 500));
+      .withProfiles("dev", "fe")
+      .withWaitStrategy("api-1", Wait.forLogMessage(/^Server started on 5000/))
+      .up();
+  }
+  catch (e) {
+    console.log(e)
+  }
+
+  console.log('finished');
+
+  await new Promise((x) => setTimeout(x, 500));
 });
